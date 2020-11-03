46e4d753d7c92d86d9b97a8d07267ff18120f886
Evan Fujita <evanfujita@gmail.com>
2020-11-03 21:00:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,16 @@
-array = Array.new
 
 def using_push(array, string)
-
+  array << string 
 end  
+
+def using_unshift(array, string)
+  array.unshift string
+end
+
+def using_pop(array)
+  array.pop
+end 
+
+def pop_with_args(array)
+  array.pop.pop
+end
